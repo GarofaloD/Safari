e1f1de2a185c2b2c8a963e4e671ef2a2d9c4d7c2
GarofaloD <garofalodaniel@gmail.com>
2020-10-15 08:27:34 -0500

    Video list created

--- Safari/Safari/App/VideoListView.swift
+++ Safari/Safari/App/VideoListView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //MARK:- PROPERTIES
+    @State var videos : [Video] = Bundle.main.decode("videos.json") //Videos
+    
+    //Haptics management
+    let hapticImact = UIImpactFeedbackGenerator(style: .medium)
+    
+    
+    
+    
+    
+    //MARK:- BODY
     var body: some View {
-        Text("Video")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }
+            }//:LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        videos.shuffle()
+                        hapticImact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+            
+        }//:NAVVIEW
     }
 }
 
+
+    //MARK:- PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
